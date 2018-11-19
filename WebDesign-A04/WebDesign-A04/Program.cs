ce0429c1b910914926efe3a112a1c72f8ea66f99
KyleRoger <kyleroger@hotmail.com>
2018-11-19 14:22:16 -0500

    First commit

---
+++ WebDesign-A04/WebDesign-A04/Program.cs
@@ -0,0 +1,25 @@
+/*
+ * 
+ * Author:      Kyle Horsley      
+ * Date:        November 19, 2018     
+ * Project:     WebDesign-A04
+ * File:        Program.cs
+ * Description: Will work as a server and get information, parse it and display an html file.
+ * 
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDesign_A04
+{
+    class MyOwnWebServer
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
